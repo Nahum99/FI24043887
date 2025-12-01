d45a7138fa5e2a321a699aff7985fc371e48f759
Nahum99 <nahumm775@gmail.com>
2025-12-01 11:55:23 -0600

    CustomTree.java

--- CP2/CustomTree.java
+++ CP2/CustomTree.java
@@ -1,5 +1,8 @@
 package cp2;
-
+// Implementación basada en explicación de estructuras de datos 
+// consultadas en:
+// https://www.geeksforgeeks.org
+// Asistencia adicional(ChatGPT)
 import java.util.Stack;
 
 public class CustomTree {
